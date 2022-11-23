13e55018373ac480e6f36967122814828b8207eb
Abinesh Jain A J <117505541+Abinesh-Jain@users.noreply.github.com>
2022-11-23 18:12:25 +0530

    Addition

---
+++ Addition/src/addition/Addition.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package addition;
+
+/**
+ *
+ * @author amsia
+ */
+public class Addition {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a=5;
+        int b=10;
+        int c=a+b;
+        System.out.println("The Sum of numbers is "+c);
+    }
+    
+}
