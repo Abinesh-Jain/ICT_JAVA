f0415cdbdc8451c18d15ea4b15afc39a3be200e7
Abinesh Jain A J <117505541+Abinesh-Jain@users.noreply.github.com>
2022-11-23 18:23:49 +0530

    Conversion

---
+++ Explicit/src/explicit/Explicit.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package explicit;
+
+/**
+ *
+ * @author amsia
+ */
+public class Explicit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double a=100.00;
+        int b=(int)a;
+        System.out.println("The number is "+b);
+    }
+    
+}
---
+++ Implicit/src/implicit/Implicit.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package implicit;
+
+/**
+ *
+ * @author amsia
+ */
+public class Implicit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a=100;
+        double b=a;
+        System.out.println("The number is "+b);
+    }
+    
+}
