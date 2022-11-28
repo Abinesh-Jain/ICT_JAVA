38218bc64c798203b16ea87778e5dedc22350258
Abinesh Jain A J <117505541+Abinesh-Jain@users.noreply.github.com>
2022-11-28 19:07:00 +0530

    Default Constructor
    Sample program on Default Constructor.

---
+++ Default_constructor/src/default_constructor/Pencil.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package default_constructor;
+
+/**
+ *
+ * @author amsia
+ */
+public class Pencil {
+    String name;
+    String shape;
+    float length;
+    float dia;
+    Pencil(){
+        name="Apsara";
+        shape="Triangle";
+        length=15;
+        dia=10;
+    }
+    String get_details(){
+        return("Name of the pencil is "+name+" and is of"+shape+" shape in cross section. The length and dia of the pencil is "+length+" cm and "+dia+" mm");
+    }
+}
