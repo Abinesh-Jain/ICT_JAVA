05397ad2038b5fce53a35a96501316b625ab71fa
Abinesh Jain A J <117505541+Abinesh-Jain@users.noreply.github.com>
2022-11-28 18:54:52 +0530

    Class & Object
    Sample program on Class & Object with JAVA Language.

---
+++ class_object/src/class_object/Class_object.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package class_object;
+
+/**
+ *
+ * @author amsia
+ */
+public class Class_object {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Pencil Apsara=new Pencil();
+        Apsara.name="Tri";
+        Apsara.shape="Triangle";
+        Apsara.length=15;
+        Apsara.dia=10;
+        System.out.println(Apsara.get_details());
+    }
+    
+}
---
+++ class_object/src/class_object/Pencil.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package class_object;
+
+/**
+ *
+ * @author amsia
+ */
+public class Pencil {
+    String name;
+    String shape;
+    float length;
+    float dia;
+    String get_details(){
+        return("Name of the pencil is "+name+" and is of"+shape+" shape in cross section. The length and dia of the pencil is "+length+" cm and "+dia+" mm");
+    }
+}
