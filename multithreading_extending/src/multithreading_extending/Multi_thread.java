37e8819841b806ca6b8ccebe7926f3cbe9930646
Abinesh Jain A J <117505541+Abinesh-Jain@users.noreply.github.com>
2022-11-29 19:33:49 +0530

    Multithreading by Extending
    New class modified by extending thread class.
    Run method is overridden.
    Start() method is used to start the thread.

---
+++ multithreading_extending/src/multithreading_extending/Multi_thread.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package multithreading_extending;
+
+/**
+ *
+ * @author amsia
+ */
+public class Multi_thread extends Thread {
+    @Override
+    public void run()
+    {
+        System.out.println("Thread in Running....");
+    }
+}
---
+++ multithreading_extending/src/multithreading_extending/Multithreading_extending.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package multithreading_extending;
+
+/**
+ *
+ * @author amsia
+ */
+public class Multithreading_extending {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Multi_thread t1= new Multi_thread();
+        t1.start();
+    }
+    
+}
