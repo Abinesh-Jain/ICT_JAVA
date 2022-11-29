20d1a290743847fdf5faf044544e30788f78cbe7
Abinesh Jain A J <117505541+Abinesh-Jain@users.noreply.github.com>
2022-11-29 20:06:57 +0530

    Basic
    Get input and give output sample.

---
+++ Basics/src/basics/Basics.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package basics;
+import java.util.*;
+/**
+ *
+ * @author amsia
+ */
+public class Basics {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner in=new Scanner(System.in);
+        System.out.println("Let's compare and find the sum, difference, product and modulus of two numbers");
+        System.out.println("Enter the First number : ");
+        int x=in.nextInt();
+        System.out.println("Enter the second number : ");
+        int y=in.nextInt();
+        System.out.println("The entered numbers are "+x+" and "+y);
+        if(x>y)
+        {
+            System.out.println(x+" is greater than "+y);
+        }else if(y>x)
+        {
+            System.out.println(y+" is greater than "+x);
+        }else
+        {
+            System.out.println("Both numbers are same or equal.");
+        }
+        int sum=x+y;
+        int diff=x-y;
+        float pro=x*y;
+        float mod=x%y;
+        System.out.println("The sum of "+x+" and "+y+" is "+sum);
+        System.out.println("The difference of "+x+" and "+y+" is "+diff);
+        System.out.println("The product of "+x+" and "+y+" is "+pro);
+        System.out.println("The modulus of "+x+" and "+y+" is "+mod);
+    }
+    
+}
