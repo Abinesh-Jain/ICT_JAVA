a39041f2f7bc286c836431952345f454467adeb7
Abinesh Jain A J <117505541+Abinesh-Jain@users.noreply.github.com>
2022-11-30 19:37:53 +0530

    Override attained

--- car_project/src/car_project/Car.java
+++ car_project/src/car_project/Car.java
@@ -8,6 +8,21 @@ package car_project;
  *
  * @author amsia
  */
-public class Car {
-    
+class vehicle
+{
+    String use;
+    void get_details(){
+        System.out.println("Car is Vehicle.");
+    }
+}
+public class Car extends vehicle{
+    String brand;
+    String model;
+    int no_of_seats;
+    int no_of_tyres;
+    @Override
+    void get_details()
+    {
+        System.out.println(model+" is of "+brand+" brand. It has "+no_of_seats+" seats and "+no_of_tyres+" tyres. It is used for "+use);
+    }
 }
--- car_project/src/car_project/Car_project.java
+++ car_project/src/car_project/Car_project.java
@@ -15,6 +15,15 @@ public class Car_project {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        vehicle road=new vehicle();
+        road.use="Transport";
+        road.get_details();
+        Car mustang=new Car();
+        mustang.brand="Ford";
+        mustang.model="Mustang GT";
+        mustang.no_of_seats=4;
+        mustang.no_of_tyres=4;
+        mustang.get_details();
     }
     
 }
