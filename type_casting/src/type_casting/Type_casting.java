54f8406be9c49460e83b63f0fd56cb4eaa80de29
Abinesh Jain A J <117505541+Abinesh-Jain@users.noreply.github.com>
2022-12-10 18:29:54 +0530

    Type Casting

---
+++ type_casting/src/type_casting/Type_casting.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package type_casting;
+
+/**
+ *
+ * @author amsia
+ */
+public class Type_casting {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double x=1.5;
+        double y=2.65;
+        sum((int)x,(int)y);
+    }
+    static void sum(int x,int y)
+    {
+        System.out.println(x+y);
+    }
+}
