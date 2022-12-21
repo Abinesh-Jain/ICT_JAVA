d5aae41a42f73d55ce86e3f476a3fcd0fdde820f
Abinesh Jain <amsiabinesh@gmail.com>
2022-12-21 09:43:42 +0530

    JAVA

---
+++ Triangle/src/triangle/main.java
@@ -0,0 +1,20 @@
+package triangle;
+
+/**
+ *
+ * @author ABINESH JAIN A J
+ */
+public class main {
+
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+    
+}
---
+++ name/src/name/Name.java
@@ -0,0 +1,16 @@
+package name;
+
+/**
+ *
+ * @author ABINESH JAIN A J
+ */
+public class Name {
+
+    public static void main(String[] args) {
+        String n[]={"Abinesh","Jain"};
+        for(String x:n)
+        {
+            System.out.println(x);
+        }
+    }
+}
