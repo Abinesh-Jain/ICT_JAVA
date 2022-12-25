b6b0345c535cb6a7609df98060ce3acc2c2ea66a
Abinesh Jain <amsiabinesh@gmail.com>
2022-12-25 21:21:17 +0530

    BACK

---
+++ Check String/src/check/string/main.java
@@ -0,0 +1,22 @@
+package check.string;
+
+/**
+ *
+ * @author ABINESH JAIN A J
+ */
+public class main {
+
+    public static void main(String[] args) {
+        String a="ABI";
+        if(a=="ABI")
+        {
+            System.out.println("OK");
+        }
+        else
+        {
+            System.out.println("NO");
+        }
+        System.out.println('a'+'b');
+    }  
+    
+}
