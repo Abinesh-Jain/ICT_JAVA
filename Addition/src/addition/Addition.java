8996063e9915cd4326892bd98a7afe398024e2e4
Abinesh Jain <amsiabinesh@gmail.com>
2022-12-27 15:00:11 +0530

    Update Addition.java

--- Addition/src/addition/Addition.java
+++ Addition/src/addition/Addition.java
@@ -16,7 +16,7 @@ public class Addition {
     public static void main(String[] args) {
         // TODO code application logic here
         int a=5;
-        int b=10;
+        int b=-10;
         int c=a+b;
         System.out.println("The Sum of numbers is "+c);
     }
