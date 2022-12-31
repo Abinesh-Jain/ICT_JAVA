161946153f3c69d2563ad0f6ae8ae85da2ce781c
Abinesh Jain <amsiabinesh@gmail.com>
2022-12-31 21:13:04 +0530

    Constructor

--- Default_constructor/src/default_constructor/Default_constructor.java
+++ Default_constructor/src/default_constructor/Default_constructor.java
@@ -1,22 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package default_constructor;
 
-/**
- *
- * @author amsia
- */
 public class Default_constructor {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
         Pencil Pencil=new Pencil();
         System.out.println(Pencil.get_details());
     }
-    
 }
--- Default_constructor/src/default_constructor/Pencil.java
+++ Default_constructor/src/default_constructor/Pencil.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package default_constructor;
 
-/**
- *
- * @author amsia
- */
 public class Pencil {
     String name;
     String shape;
