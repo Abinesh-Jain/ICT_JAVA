e6aa8f626bb063468f738f97ff1f4ead21e59b34
Abinesh Jain <amsiabinesh@gmail.com>
2023-01-13 19:36:56 +0530

    Update If_else.java

--- if_else/src/if_else/If_else.java
+++ if_else/src/if_else/If_else.java
@@ -1,20 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package if_else;
 
-/**
- *
- * @author amsia
- */
 public class If_else {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
         int Tamil=97;
         int English=96;
         int Maths=87;
