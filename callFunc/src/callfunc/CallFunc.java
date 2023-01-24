cfdbf36c2ed6ae1bbb220345a2bc0c2d8e27fb00
Abinesh Jain <amsiabinesh@gmail.com>
2023-01-24 17:34:50 +0530

    update

---
+++ callFunc/src/callfunc/CallFunc.java
@@ -0,0 +1,9 @@
+package callfunc;
+public class CallFunc {
+    static String say(){
+        return "Hello";
+    }
+    public static void main(String[] args) {
+        System.out.println(say());
+    }
+}
