98f38d3d267b90fc4d830c828c2b2b10a3829f7e
Abinesh Jain <amsiabinesh@gmail.com>
2023-02-07 10:52:09 +0530

    Update Animal_Inteface.java

--- Animal_Inteface/src/animal_inteface/Animal_Inteface.java
+++ Animal_Inteface/src/animal_inteface/Animal_Inteface.java
@@ -1,18 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package animal_inteface;
 
-/**
- *
- * @author amsia
- */
 public class Animal_Inteface {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
         animal Dog=new dog();
@@ -21,6 +10,7 @@ public class Animal_Inteface {
         Dog.play();
         Cat.swim();
         Cat.play();
+        System.out.println(Dog);
     }
     
 }
